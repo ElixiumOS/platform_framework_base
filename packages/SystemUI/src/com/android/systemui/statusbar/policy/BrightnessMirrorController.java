DirtyUnicorns/android_frameworks_base

Add auto brightness toggle to Quick Settings
- Not a new tile, just a small icon/toggle beside the brightness slider

Change-Id: I9632532aa3558911e6e7c8718feddc83e7bcfac6

packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -21,8 +21,13 @@ import com.android.systemui.statusbar.ScrimView;
 import com.android.systemui.statusbar.phone.PhoneStatusBar;
 import com.android.systemui.statusbar.phone.StatusBarWindowView;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
+import android.widget.ImageView;
 import android.widget.FrameLayout;
 
 /**
@@ -37,14 +42,21 @@ public class BrightnessMirrorController {
     private final View mBrightnessMirror;
     private final View mPanelHolder;
     private final int[] mInt2Cache = new int[2];
+    private final ImageView mIcon;
+    private Context mContext;
 
-    public BrightnessMirrorController(View statusBarWindow) {
+    public BrightnessMirrorController(Context context, View statusBarWindow) {
+        mContext = context;
         mScrimBehind = (ScrimView) statusBarWindow.findViewById(R.id.scrim_behind);
         mBrightnessMirror = statusBarWindow.findViewById(R.id.brightness_mirror);
         mPanelHolder = statusBarWindow.findViewById(R.id.panel_holder);
+        mIcon = (ImageView) statusBarWindow.findViewById(R.id.brightness_icon);
+        // enable the brightness icon
+        mIcon.setVisibility(View.VISIBLE);
     }
 
     public void showMirror() {
+        updateIcon();
         mBrightnessMirror.setVisibility(View.VISIBLE);
         mScrimBehind.animateViewAlpha(0.0f, TRANSITION_DURATION_OUT, PhoneStatusBar.ALPHA_OUT);
         outAnimation(mPanelHolder.animate())
@@ -108,5 +120,17 @@ public class BrightnessMirrorController {
                 R.dimen.notification_side_padding);
         mBrightnessMirror.setPadding(padding, mBrightnessMirror.getPaddingTop(),
                 padding, mBrightnessMirror.getPaddingBottom());
+    }
+
+    private void updateIcon() {
+        if (mIcon != null) {
+            boolean automatic = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                    UserHandle.USER_CURRENT) != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+            mIcon.setImageResource(automatic ?
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on :
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+        }
     }
 }

ElixiumOS/platform_framework_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/BrightnessMirrorController.java
@@ -17,8 +17,14 @@
 package com.android.systemui.statusbar.policy;
 
 import android.view.LayoutInflater;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
+import android.widget.ImageView;
 import android.widget.FrameLayout;
 
 import com.android.systemui.Interpolators;
@@ -41,17 +47,24 @@ public class BrightnessMirrorController {
     private final View mNotificationPanel;
     private final int[] mInt2Cache = new int[2];
     private View mBrightnessMirror;
+    private final ImageView mIcon;
+    private Context mContext;
 
-    public BrightnessMirrorController(StatusBarWindowView statusBarWindow) {
+    public BrightnessMirrorController(Context context, StatusBarWindowView statusBarWindow) {
+        mContext = context;
         mStatusBarWindow = statusBarWindow;
         mScrimBehind = (ScrimView) statusBarWindow.findViewById(R.id.scrim_behind);
         mBrightnessMirror = statusBarWindow.findViewById(R.id.brightness_mirror);
         mNotificationPanel = statusBarWindow.findViewById(R.id.notification_panel);
         mStackScroller = (NotificationStackScrollLayout) statusBarWindow.findViewById(
                 R.id.notification_stack_scroller);
+        mIcon = (ImageView) statusBarWindow.findViewById(R.id.brightness_icon);
+        // enable the brightness icon
+        mIcon.setVisibility(View.VISIBLE);
     }
 
     public void showMirror() {
+        updateIcon();
         mBrightnessMirror.setVisibility(View.VISIBLE);
         mStackScroller.setFadingOut(true);
         mScrimBehind.animateViewAlpha(0.0f, TRANSITION_DURATION_OUT, Interpolators.ALPHA_OUT);
@@ -121,5 +134,17 @@ public class BrightnessMirrorController {
         mBrightnessMirror = LayoutInflater.from(mBrightnessMirror.getContext()).inflate(
                 R.layout.brightness_mirror, mStatusBarWindow, false);
         mStatusBarWindow.addView(mBrightnessMirror, index);
+    }
+
+    private void updateIcon() {
+        if (mIcon != null) {
+            boolean automatic = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+                    UserHandle.USER_CURRENT) != Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL;
+            mIcon.setImageResource(automatic ?
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_on :
+                    com.android.systemui.R.drawable.ic_qs_brightness_auto_off);
+        }
     }
 }